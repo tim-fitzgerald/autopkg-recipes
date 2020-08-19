7b12d27b51b2e061ac8c62b8fc1fae7cdd859696
tim-fitzgerald <tim.fitzgerald@clio.com>
2020-08-19 10:24:52 -0700

    Black formatted

--- PostProcessors/Slacker.py
+++ PostProcessors/Slacker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #!/usr/bin/python
-# 
+#
 # Copyright 2019 Tim Fitzgerald
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script was taken more or less verbatim from Ben Reilly - but it had a typo that caused an error. I've fixed it but 
-# Bens original repo doesn't seem to be online anymore for me to make a PR. 
+# This script was taken more or less verbatim from Ben Reilly - but it had a typo that caused an error. I've fixed it but
+# Bens original repo doesn't seem to be online anymore for me to make a PR.
 
 from autopkglib import Processor, ProcessorError, URLGetter
 
@@ -29,31 +29,31 @@ import json
 
 __all__ = ["Slacker"]
 
+
 class Slacker(URLGetter):
-    description = ("Posts to Slack via webhook based on output of a MunkiImporter. "
-                    "Based on Graham Pugh's slacker.py - https://github.com/grahampugh/recipes/blob/master/PostProcessors/slacker.py"
-                    "and "
-                    "@thehill idea on macadmin slack - https://macadmins.slack.com/archives/CBF6D0B97/p1542379199001400"
-                    "Takes elements from " "https://gist.github.com/devStepsize/b1b795309a217d24566dcc0ad136f784"
-                    "and "
-                    "https://github.com/autopkg/nmcspadden-recipes/blob/master/PostProcessors/Yo.py")
+    description = (
+        "Posts to Slack via webhook based on output of a MunkiImporter. "
+        "Based on Graham Pugh's slacker.py - https://github.com/grahampugh/recipes/blob/master/PostProcessors/slacker.py"
+        "and "
+        "@thehill idea on macadmin slack - https://macadmins.slack.com/archives/CBF6D0B97/p1542379199001400"
+        "Takes elements from "
+        "https://gist.github.com/devStepsize/b1b795309a217d24566dcc0ad136f784"
+        "and "
+        "https://github.com/autopkg/nmcspadden-recipes/blob/master/PostProcessors/Yo.py"
+    )
 
     input_variables = {
         "munki_info": {
             "required": False,
-            "description": ("Munki info dictionary to use to display info.")
+            "description": ("Munki info dictionary to use to display info."),
         },
         "munki_repo_changed": {
             "required": False,
-            "description": ("Whether or not item was imported.")
+            "description": ("Whether or not item was imported."),
         },
-        "webhook_url": {
-            "required": False,
-            "description": ("Slack webhook.")
-        }
+        "webhook_url": {"required": False, "description": ("Slack webhook.")},
     }
-    output_variables = {
-    }
+    output_variables = {}
 
     __doc__ = description
 
@@ -70,25 +70,33 @@ class Slacker(URLGetter):
         if was_imported:
             name = self.env.get("munki_importer_summary_result")["data"]["name"]
             version = self.env.get("munki_importer_summary_result")["data"]["version"]
-            pkg_path = self.env.get("munki_importer_summary_result")["data"]["pkg_repo_path"]
-            pkginfo_path = self.env.get("munki_importer_summary_result")["data"]["pkginfo_path"]
+            pkg_path = self.env.get("munki_importer_summary_result")["data"][
+                "pkg_repo_path"
+            ]
+            pkginfo_path = self.env.get("munki_importer_summary_result")["data"][
+                "pkginfo_path"
+            ]
             catalog = self.env.get("munki_importer_summary_result")["data"]["catalogs"]
             if name:
-                slack_text = "*New item added to repo:*\nTitle: *%s*\nVersion: *%s*\nCatalog: *%s\n*Pkg Path: *%s*\nPkginfo Path: *%s*" % (name, version, catalog, pkg_path, pkginfo_path)
-                slack_data = json.dumps({'text': slack_text, 'icon_url': AUTOPKGICON, 'username': USERNAME})
-                headers = {
-                    "Content-Type": "application/json"
-                }
+                slack_text = (
+                    "*New item added to repo:*\nTitle: *%s*\nVersion: *%s*\nCatalog: *%s\n*Pkg Path: *%s*\nPkginfo Path: *%s*"
+                    % (name, version, catalog, pkg_path, pkginfo_path)
+                )
+                slack_data = json.dumps(
+                    {"text": slack_text, "icon_url": AUTOPKGICON, "username": USERNAME}
+                )
+                headers = {"Content-Type": "application/json"}
                 curl_opts = [
-                    "--request", "POST",
-                    "--data", slack_data,
-                    "{}".format(self.env.get("webhook_url"))
+                    "--request",
+                    "POST",
+                    "--data",
+                    slack_data,
+                    "{}".format(self.env.get("webhook_url")),
                 ]
                 try:
                     curl_cmd = self.prepare_curl_cmd()
                     self.add_curl_headers(curl_cmd, headers)
                     curl_cmd.extend(curl_opts)
-                    print ("Curl command is:", curl_cmd)
                     response = self.download_with_curl(curl_cmd)
 
                 except:
